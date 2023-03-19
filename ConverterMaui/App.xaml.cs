97f383278466ba743b67694694fffb41ba38f654
Luis Inacio <lrs.inacio@gmail.com>
2023-03-19 23:57:30 +0000

    Finished product, PropertyChanged.Fody and UnitsNet dependencies used

--- ConverterMaui/App.xaml.cs
+++ ConverterMaui/App.xaml.cs
@@ -1,11 +1,13 @@
-﻿namespace ConverterMaui;
+﻿using ConverterMaui.MVVM.Views;
+
+namespace ConverterMaui;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new NavigationPage(new MenuView());
+    }
 }
